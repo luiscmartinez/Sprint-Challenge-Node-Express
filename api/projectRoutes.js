fe26f2d43d9c2a9ee0ee0b6489bf066397ea7214
Luis <luis@Luiss-MacBook-Air.local>
2018-07-27 09:31:03 -0700

    created endpoint to get a project by ID

--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -1,7 +1,21 @@
 const server = require('express')()
 const projectDb = require('../data/helpers/projectModel')
 
+// GET ALL PROJECTS
 server.get('/', (req, res, next) => {
   projectDb.get().then((projects) => res.status(200).json(projects)).catch(next)
 })
+
+// GET A PROJECT BY ID
+server.get('/:id', (req, res, next) => {
+  projectDb
+    .get(req.params.id)
+    .then((project) => {
+      project === undefined
+        ? next(new Error('CANT_FIND_PRO'))
+        : res.status(200).json(project)
+    })
+    .catch(next)
+})
+
 module.exports = server
