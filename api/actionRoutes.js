ee14e9252d494b1b1634c865a9fcda03b496d2dc
Luis <luis@Luiss-MacBook-Air.local>
2018-07-27 10:42:31 -0700

    created endpoint to get an action by ID

--- api/actionRoutes.js
+++ api/actionRoutes.js
@@ -1,7 +1,21 @@
 const server = require('express')()
 const actionDb = require('../data/helpers/actionModel')
 
+// GET ALL ACTIONS
 server.get('/', (req, res, next) => {
   actionDb.get().then((actions) => res.status(200).json(actions)).catch(next)
 })
+
+// GET ACTION BY ID
+server.get('/:id', (req, res, next) => {
+  actionDb
+    .get(req.params.id)
+    .then((action) => {
+      action === undefined
+        ? next(new Error('CANT_FIND_ITEM'))
+        : res.status(200).json(action)
+    })
+    .catch(next)
+})
+
 module.exports = server
