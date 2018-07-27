ac71de9d576e9db71a7c6f9ebed498aae21f98b7
Luis <luis@Luiss-MacBook-Air.local>
2018-07-27 09:34:37 -0700

    created error handling middleware to catch all errors

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -4,4 +4,17 @@ const actionRoutes = require('./actionRoutes')
 module.exports = (server) => {
   server.use('/api/projects', projectRoutes)
   server.use('/api/actions', actionRoutes)
+  server.use((err, req, res, next) => {
+    switch (err.message) {
+      case 'CANT_FIND_PRO':
+        res
+          .status(404)
+          .send({ status: 'There is no existing project by that ID' })
+        break
+
+      default:
+        res.status(500).send({ error: err.message })
+        break
+    }
+  })
 }
